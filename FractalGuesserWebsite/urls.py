a668eba704562da79f8ff00ebc6e49ecb3112b9e
Douglas Hindson <dmhindson@gmail.com>
2014-12-07 16:09:02 +0800

    Made default pages use cdns for jquery, underscorejs, backbonejs, etc. Created page for China and offline use.

--- FractalGuesserWebsite/urls.py
+++ FractalGuesserWebsite/urls.py
@@ -4,5 +4,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = patterns('',
   url(r'^mandelbrot/', 'FractalGuesserWebsite.views.mandelbrot', name='mandelbrot'),
   url(r'^juliet/', 'FractalGuesserWebsite.views.juliet', name='juliet'),
+  url(r'^chinahome/', 'FractalGuesserWebsite.views.china_home_page', name='china_home_page'),
 )
 urlpatterns += staticfiles_urlpatterns()
--- FractalGuesserWebsite/views.py
+++ FractalGuesserWebsite/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def home_page(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html')	
+	
+def china_home_page(request):
+	return render(request, 'china_home.html')
 	
 def juliet(request):
 	return render(request, 'juliet.html')
